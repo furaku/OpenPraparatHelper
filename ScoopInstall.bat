ce365b53e308834165bcea03faed0850543d6f99
nodokaha <scpeee@wail.ch>
2023-10-20 22:32:24 +0900

    "OpenPraparatHelperのscoopによる自動インストールスクリプト追加"

---
+++ ScoopInstall.bat
@@ -0,0 +1,3 @@
+pwsh -ep RemoteSigned -Command "irm get.scoop.sh | iex"
+pwsh -Command "scoop bucket add nodoka https://github.com/nodokaha/myscoop"
+pwsh -Command "scoop install OpenPraparatHelper"
