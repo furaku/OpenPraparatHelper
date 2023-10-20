2cd142fec8ca6721055fbccd93a818ca76aa0c81
nodokaha <scpeee@wail.ch>
2023-10-20 22:51:46 +0900

    READMEにscoopインストールについて追記

--- ScoopInstall.bat
+++ ScoopInstall.bat
@@ -1,3 +1,4 @@
+winget install Microsoft.PowerShell
 pwsh -ep RemoteSigned -Command "irm get.scoop.sh | iex"
 pwsh -Command "scoop bucket add nodoka https://github.com/nodokaha/myscoop"
 pwsh -Command "scoop install OpenPraparatHelper"
